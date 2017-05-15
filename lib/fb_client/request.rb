6765269bb81d1af9999fef3316430fc7f57f5209
Tomáš Hrabal <fil.hrabal@gmail.com>
2017-05-15 13:04:37 +0200

    [FIX] Errors

--- lib/fb_client/request.rb
+++ lib/fb_client/request.rb
@@ -7,23 +7,14 @@ class FbClient
       attempts: 2,
       retry_wait: 3 }.freeze
 
-    def self.ua_get(url, connect_params, params = DEFAULT_PARAMS)
-      tries = params[:attempts]
-      begin
-        response = Typhoeus.get(url, connect_params)
-      rescue FetchFailedException => error
-        tries -= 1
-        return false if tries.zero?
-        sleep(params[:retry_wait])
-        retry
-      end
-
+    def self.ua_get(url, connect_params, _ = {})
+      response = Typhoeus.get(url, connect_params)
       return { error: response.return_message, content: response.body } unless
         response.success?
 
       begin
         Oj.load(response.body)
-      rescue SyntaxError, Oj::ParseError => e
+      rescue SyntaxError, Oj::ParseError
         nil
       end
     end
